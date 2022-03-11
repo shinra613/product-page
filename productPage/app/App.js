44a152f345ff21d383d8981f42768132635dc4ac
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2022-03-11 05:58:47 +0530

    refactor

--- productPage/app/App.js
+++ productPage/app/App.js
@@ -1,60 +1,115 @@
 
-// let testData = [
-//     {
-//         "price": "15.00",
-//         "Product_Name": "NutraBay Energy",
-//         "ID": "4221105000000015011",
-//         "type": "preworkout",
-//         "Image": "/api/v2/buildsandbox/product-page/report/All_Products/4221105000000015011/Image/download?filepath=1646834118934_1.jpg"
-//     },
-//     {
-//         "price": "30.00",
-//         "Product_Name": "BigMuscel Energy",
-//         "ID": "4221105000000015007",
-//         "type": "preworkout",
-//         "Image": "/api/v2/buildsandbox/product-page/report/All_Products/4221105000000015007/Image/download?filepath=1646834068904_3.jpg"
-//     },
-//     {
-//         "price": "20.00",
-//         "Product_Name": "Amino",
-//         "ID": "4221105000000015003",
-//         "type": "preworkout",
-//         "Image": "/api/v2/buildsandbox/product-page/report/All_Products/4221105000000015003/Image/download?filepath=1646834027810_2.jpg"
-//     }
-// ]
+let testData = [
+    {
+        "price": "15.00",
+        "Product_Name": "NutraBay Energy",
+        "ID": "4221105000000015011",
+        "type": "preworkout",
+        "Image": "/api/v2/buildsandbox/product-page/report/All_Products/4221105000000015011/Image/download?filepath=1646834118934_1.jpg"
+    },
+    {
+        "price": "30.00",
+        "Product_Name": "BigMuscel Energy",
+        "ID": "4221105000000015007",
+        "type": "preworkout",
+        "Image": "/api/v2/buildsandbox/product-page/report/All_Products/4221105000000015007/Image/download?filepath=1646834068904_3.jpg"
+    },
+    {
+        "price": "20.00",
+        "Product_Name": "Amino",
+        "ID": "4221105000000015003",
+        "type": "preworkout",
+        "Image": "/api/v2/buildsandbox/product-page/report/All_Products/4221105000000015003/Image/download?filepath=1646834027810_2.jpg"
+    }
+]
+
+let dummyImg = './1.jpg'
 
 
+const plus=()=>{
 
-// testData.map((i) => {
-//     let imgName = i.Image.split("=");
-//     let imgURL = 'https://creatorexport.zoho.com/file/buildsandbox/product-page/All_Products/' + i.ID + '/image-download?filepath=/' + imgName[1];
-//     console.log(imgURL);
-//     console.log(imgName);
-//     const parent = document.getElementById('product-display');
-//     const child = document.createElement("div");
-//     const subChild = document.createElement("div");
-//     const childImage = document.createElement("img");
-//     const childName = document.createElement("h3");
-//     const childPrice = document.createElement("h4");
-//     const childCheckout = document.createElement("button");
-//     childCheckout.innerHTML= "Checkout"
-//     const namenode = document.createTextNode(i.Product_Name);
-//     const pricenode = document.createTextNode('$ '+i.price);
-//     childName.appendChild(namenode);
-//     childPrice.appendChild(pricenode);
-//     childImage.src = i.Image;
-//     childImage.loading="lazy"
-//     child.className = "product-card";
-//     subChild.className = "product-desc";
+
+    console.log('pluss');
+
+// let exquantity = document.querySelector("");
+// newValue =  exquantity.value+1; 
+// exquantity.setAttribute("value",newValue);
+
+
+}
+
+
+const minus=()=>{
+
+
+    console.log('minuss');
+
+// let exquantity = document.querySelector("");
+// newValue =  exquantity.value+1; 
+// exquantity.setAttribute("value",newValue);
+
+
+}
+
+
+testData.map((i) => {
+
+    let imgName = i.Image.split("=");
+    let imgURL = 'https://creatorexport.zoho.com/file/buildsandbox/product-page/All_Products/' + i.ID + '/Image/image-download?filepath=/' + imgName[1];
+    console.log(imgURL);
+    console.log(imgName);
+    const parent = document.getElementById('product-display');
+    const child = document.createElement("div");
+    const subChild = document.createElement("div");
+    const childImage = document.createElement("img");
+    const childName = document.createElement("h3");
+    const childPrice = document.createElement("h4");
+    const childCheckout = document.createElement("button");
+    const childquantity = document.createElement("div");
+
+
+    childquantity.className="product-quantity";
+    const quanplus = document.createElement("span");
+    quanplus.className = "plus"+i.ID;
+    quanplus.innerHTML="+"
+    quanplus.addEventListener("click", plus);
+    const quantity = document.createElement("input");
+    quantity.className = "quantity"+i.ID;
+    quantity.setAttribute("type", "text");
+    quantity.setAttribute("value", 0);
+    const quanminus = document.createElement("span");
+    quanminus.className = "minus"+i.ID;
+    quanminus.innerHTML="-"
+    quanminus.addEventListener("click", minus);
+    childquantity.appendChild(quanplus);
+    childquantity.appendChild(quantity);
+    childquantity.appendChild(quanminus);
+
+    childCheckout.innerHTML= "Add to cart"
+    const namenode = document.createTextNode(i.Product_Name);
+    const pricenode = document.createTextNode('$ '+i.price);
+    childName.appendChild(namenode);
+    childPrice.appendChild(pricenode);
+    childImage.src = dummyImg;
+    childImage.loading="lazy"
+    child.className = "product-card";
+    subChild.className = "product-desc";
    
-//     subChild.appendChild(childName);
-//     subChild.appendChild(childPrice);
-//     subChild.appendChild(childCheckout);
-//     child.appendChild(childImage);
-//     child.appendChild(subChild);
-//     parent.appendChild(child);
+    subChild.appendChild(childName);
+    subChild.appendChild(childPrice);
+    subChild.appendChild(childquantity);
+    subChild.appendChild(childCheckout);
+    child.appendChild(childImage);
+    child.appendChild(subChild);
+    parent.appendChild(child);
 
-// });
+    
+
+});
+
+
+
+
 
 
 
